382cd2ef78e3bb565800d1796744e0957f64b671
d3xt3r0 <prathapss87@gmail.com>
2022-06-08 18:51:25 +0530

    Add user login facility
    Need to add user registration

---
+++ README.md
@@ -0,0 +1,10 @@
+# Thoughts Update
+
+## A minimal django based app to put your thoughts into the interweb
+
+
+# TODO
+
+- [x] Beautify the form
+- [ ] Implement Auth
+- [ ] Implement crypto payment
--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth import get_user
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import get_user, authenticate, login, logout
 from .models import Post
 from .forms import PostForm
 
@@ -8,16 +10,41 @@ from .forms import PostForm
 
 def index(request):
 
-    current_user = get_user(request)
-    form = PostForm(initial={'author':current_user})
+    current_user = request.user
+    form = PostForm()
 
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            form.save()
+            form_model = form.save(commit=False)
+            form_model.author= current_user
+            form_model.save()
             return redirect('index')
 
     posts = Post.objects.all().order_by('-created_date')
     context = {'posts': posts, 'form': form}
     return render(request, 'homepage/home.html', context)
+
+
+def login_view(request):
+
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Username or password, incorrect')
+
+    context = {}
+    return render(request, 'homepage/login_register.html', context)
 
